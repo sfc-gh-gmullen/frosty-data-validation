607921860aa0eba957c85180b2f00d1a08576b38
Brock Cooper <brock.cooper@snowflake.com>
2021-01-28 17:17:09 -0700

    fix weird spacing

--- Method2/method_2_example.sql
+++ Method2/method_2_example.sql
@@ -1,14 +1,13 @@
 /*
-    Create a schema that will hold all your validation views (1 view for each
-    test)
+    Create a schema that will hold all your validation views (1 view for each test)
 */
 
 CREATE SCHEMA IF NOT EXISTS validation;
 
 /*
-    Alternatively, for an organization that might have hundreds of tests, you may want to
-    create an entire "validation" database, with many schemas for additional
-    organization. For example:
+    Alternatively, for an organization that might have hundreds of tests, you
+    may want to create an entire "validation" database, with many schemas for
+    additional organization. For example:
 
     CREATE DATBASE validation;
     CREATE SCHEMA citibike_valiadtion;
